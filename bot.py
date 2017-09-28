81eff2f12d5c7dac8630f1c48102a3c5f0aca9ee
WORD559 <josh.barrass.work@gmail.com>
2017-09-28 20:45:04 +0100

    A few minor bug fixes and adding the code for sending messages.

--- bot.py
+++ bot.py
@@ -16,20 +16,31 @@ class Bot(object):
     def start(self,route="/incoming"):
         @app.route(route,methods=["POST","GET"])
         def main():
-            print self
-            if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
+            if not self.kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
                 return Response(status=403)
 
             messages = messages_from_json(request.json['messages'])
             print "--Received Messages",messages
             to_send = None
             for message in messages:
-                split = message.body.split(" ")
-                command = split[0]
-                text_data = split[1:]
-
+                if isinstance(message,TextMessage):
+                    split = message.body.split(" ")
+                    command = split[0]
+                    text_data = " ".join(split[1:])
+                    if self.functions.has_key(command):
+                        r = self.functions[command](text_data)
+                        for m in r:
+                            if m.to == None:
+                                m.to = messasge.from_user
+                            if m.chat_id == None:
+                                m.chat_id = message.chat_id
+                    else:
+                        r = [TextMessage(
+                            to=message.from_user,
+                            chat_id=message.chat_id,
+                            body="Unknown command.")]
+                    self.kik.send_messages(r)
             return Response(status=200)
-
                 
         
     def command(self,name,help_entry=None):
