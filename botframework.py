e65663105cd4e6927dd7505d0f7c135fe982b83f
WORD559 <josh.barrass.work@gmail.com>
2017-09-28 21:42:22 +0100

    Added support for suggested response keyboards

--- botframework.py
+++ botframework.py
@@ -3,18 +3,20 @@ from kik import KikApi, Configuration
 from kik.messages import messages_from_json, TextMessage, PictureMessage,\
      PresetAttributions, SuggestedResponseKeyboard, TextResponse,\
      LinkMessage, CustomAttribution, VideoMessage, StartChattingMessage,\
-     StickerMessage, ScanDataMessage
+     StickerMessage, ScanDataMessage, IsTypingMessage
 
 app = Flask(__name__)
 
 class Bot(object):
-    def __init__(self,username,api_key,webhook):
+    def __init__(self,username,api_key,webhook,case_sensitive=False):
         self.functions = {}
         self.help = {}
+        self.keyboard_entries = []
         self.kik = KikApi(username, api_key)
         self.kik.set_configuration(Configuration(
             webhook=webhook
         ))
+        self.case_sensitive = case_sensitive
 
     def start(self,route="/incoming"):
         @app.route(route,methods=["POST","GET"])
@@ -26,9 +28,16 @@ class Bot(object):
             print "--Received Messages",messages
             to_send = None
             for message in messages:
+                self.kik.send_messages([IsTypingMessage(
+                    to=message.from_user,
+                    chat_id=message.chat_id,
+                    is_typing=True)])
+                
                 if isinstance(message,TextMessage):
                     split = message.body.split(" ")
                     command = split[0]
+                    if not self.case_sensitive:
+                        command = command.lower()
                     text_data = " ".join(split[1:])
                     if self.functions.has_key(command):
                         r = self.functions[command](text_data)
@@ -37,6 +46,11 @@ class Bot(object):
                                 m.to = message.from_user
                             if m.chat_id == None:
                                 m.chat_id = message.chat_id
+                            if m.keyboards == []:
+                                keyboard = self.make_keyboard()
+                                if len(keyboard.responses) > 0:
+                                    m.keyboards.append(keyboard)
+                                    
                     else:
                         r = [TextMessage(
                             to=message.from_user,
@@ -44,15 +58,31 @@ class Bot(object):
                             body="Unknown command.")]
                     self.kik.send_messages(r)
             return Response(status=200)
+
+    def make_keyboard(self):
+        keyboard = SuggestedResponseKeyboard(
+            hidden=False,
+            responses=[TextResponse(x) for x in self.keyboard_entries])
+        return keyboard
                 
         
     def command(self,name,help_entry=None):
+        if not self.case_sensitive:
+            name = name.lower()
         def decorator(function):
             def wrapper():
                 return function
             self.functions[name] = function
             if isinstance(help_entry,str):
                 self.help[name] = help_entry
+            return wrapper()
+        return decorator
+
+    def keyboard(self,entry):
+        def decorator(function):
+            def wrapper():
+                return function
+            self.keyboard_entries.append(entry)
             return wrapper()
         return decorator
 
